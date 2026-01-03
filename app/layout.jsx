fb2a12e525d8bfd8b8b0247bd165babf7ae7f975
RohitGuptaRaja <rohitkumardajar@gmail.com>
2026-01-03 18:59:07 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,6 +1,7 @@
 import { Quicksand } from "next/font/google"
 import "./globals.css"
-import BackgroundMusic from "../components/BackgroundMusic"
+import BackgroundMusic from "@/components/BackgroundMusic"
+import Decorations from "@/components/Decorations"
 
 const quicksand = Quicksand({
   subsets: ["latin"],
@@ -8,15 +9,16 @@ const quicksand = Quicksand({
 })
 
 export const metadata = {
-  title: "Happy Birthday Anjali 💖",
-  description: "A special birthday surprise",
+  title: "Happy Birthday 💖",
+  description: "A special birthday surprise made with love",
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`${quicksand.className} bg-black antialiased`}>
+      <body className={`${quicksand.className} bg-black text-white overflow-hidden`}>
         <BackgroundMusic />
+        <Decorations />
         {children}
       </body>
     </html>
