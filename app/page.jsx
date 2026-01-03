9c8f005736e93c0a19f7ce4edbd62adb3d163bf9
RohitGuptaRaja <rohitkumardajar@gmail.com>
2026-01-03 19:05:45 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -1,22 +1,15 @@
 "use client"
-import { motion } from "framer-motion"
+import { useState } from "react"
+import CakeScreen from "@/components/CakeScreen"
 
 export default function Home() {
+  const [screen, setScreen] = useState(1)
+
   return (
-    <main className="h-screen w-screen flex items-center justify-center">
-      <motion.div
-        initial={{ scale: 0.8, opacity: 0 }}
-        animate={{ scale: 1, opacity: 1 }}
-        transition={{ duration: 1 }}
-        className="glass-card text-center px-6 py-10"
-      >
-        <h1 className="text-3xl font-bold text-pink-400">
-          Birthday Surprise 🎂
-        </h1>
-        <p className="mt-3 text-gray-300">
-          Part 1 setup completed 💖
-        </p>
-      </motion.div>
-    </main>
+    <>
+      {screen === 1 && (
+        <CakeScreen onNext={() => setScreen(2)} />
+      )}
+    </>
   )
 }
